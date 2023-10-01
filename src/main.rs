aa8ddedb1f3b223ae47418068d003aab65ef2a17
unknown <nashybamaksym@gmail.com>
2023-10-01 22:46:10 +0200

    player movement

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 mod player;
-
 mod traffic;
 mod camera;
 
 use bevy::prelude::*;
 use crate::traffic::TrafficPlugin;
 use crate::camera::CameraPlugin;
+use crate::player::PlayerPlugin;
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, CameraPlugin, TrafficPlugin))
+        .add_plugins((DefaultPlugins, CameraPlugin, TrafficPlugin, PlayerPlugin))
         .run();
 }
--- src/player.rs
+++ src/player.rs
@@ -1,7 +1,11 @@
-use bevy::app::{App, Plugin, Startup};
+use bevy::app::{App, Plugin, Startup, Update};
 use bevy::asset::AssetServer;
-use bevy::prelude::{Commands, Res, Transform};
+use bevy::core::Zeroable;
+use bevy::input::Input;
+use bevy::math::{Vec2, Vec3};
+use bevy::prelude::{Commands, Component, KeyCode, Query, Res, Transform, With};
 use bevy::sprite::{SpriteBundle};
+use bevy::time::Time;
 use bevy::utils::default;
 
 pub struct PlayerPlugin;
@@ -9,19 +13,57 @@ pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin{
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, spawn_player);
+        app.add_systems(Update, move_player);
     }
 }
 
-fn spawn_player(mut commands : Commands, asset_server: Res<AssetServer>){
-    let player = SpriteBundle{
-        transform: Transform::from_xyz(0f32, 0f32, 0f32),
+#[derive(Component)]
+struct Player{
+    speed : Vec3
+}
+
+fn spawn_player(
+        mut commands : Commands,
+        asset_server: Res<AssetServer>){
+
+    let player = (SpriteBundle{
+        transform: Transform::from_scale(Vec3{
+            x: 0.1f32,
+            y: 0.1f32,
+            z: 0.1f32,
+        }),
         texture: asset_server.load("gosling.png"),
         ..default()
-    };
+    }, Player{speed:Vec3{
+        x: 20f32,
+        y: 100f32,
+        z: 0.0,
+    }});
 
     commands.spawn(player);
 }
 
-fn move_player(mut commands : Commands){
+fn move_player(
+        keys : Res<Input<KeyCode>>,
+        time : Res<Time>,
+        mut player_q : Query<& Player>,
+        mut player_trans_q : Query<&mut Transform, With<Player>>){
 
+    let mut player_transform = player_trans_q.get_single_mut().unwrap();
+    let mut direction = Vec3::zeroed();
+
+    if keys.pressed(KeyCode::D){
+        direction += Vec3{x:1f32, y:0f32, z:0f32};
+    }
+    if keys.pressed(KeyCode::W){
+        direction += Vec3{x:0f32, y:1f32, z:0f32};
+    }
+    if keys.pressed(KeyCode::A){
+        direction += Vec3{x:-1f32, y:0f32, z:0f32};
+    }
+    if keys.pressed(KeyCode::S){
+        direction += Vec3{x:0f32, y:-1f32, z:0f32};
+    }
+
+    player_transform.translation += direction * player_q.get_single().unwrap().speed * time.delta_seconds();
 }
