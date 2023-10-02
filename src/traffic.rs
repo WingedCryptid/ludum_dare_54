a37a84c8d2cc62421bc0911b65f0010fc5061a34
WingedCryptid <yariksouth@gmail.com>
2023-10-02 08:43:32 +0300

    Added offenders

--- src/traffic.rs
+++ src/traffic.rs
@@ -1,63 +1,99 @@
 use bevy::prelude::*;
-use bevy::sprite::MaterialMesh2dBundle;
-use crate::common::MovingObject;
-use crate::game::Game;
 use std::ops::Range;
 use rand::Rng;
+use crate::common::MovingObject;
+use crate::game::Game;
 
 pub struct TrafficPlugin;
 
 impl Plugin for TrafficPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_traffic)
-            .add_systems(Update, (move_traffic, try_cross_dead_zone));
+        let traffic_settings = TrafficSettings {
+            unit_speed: (0.75..1.0),
+            offender_speed: (1.1..1.5),
+            unit_spawn_x: (0.0..50.0),
+            unit_spawn_y: (-250.0..250.0),
+            units_count: 8,
+            offenders_count: 5,
+        };
+
+        app.insert_resource(traffic_settings)
+        .add_systems(Startup, spawn_traffic)
+        .add_systems(Update, (move_traffic, try_cross_dead_zone));
     }
 }
 
 #[derive(Component)]
-struct TrafficUnit;
+struct Offender;
 
 #[derive(Component)]
+struct TrafficUnit;
+
+#[derive(Bundle)]
+struct OffenderBundle {
+    traffic_unit_bundle: TrafficUnitBundle,
+    offender: Offender,
+}
+
+#[derive(Bundle)]
+struct TrafficUnitBundle {
+    sprite_bundle: SpriteBundle,
+    traffic_unit: TrafficUnit,
+    moving_object: MovingObject,
+}
+
+#[derive(Resource)]
 struct TrafficSettings {
     unit_speed: Range<f32>,
+    offender_speed: Range<f32>,
     unit_spawn_x: Range<f32>,
     unit_spawn_y: Range<f32>,
+    units_count: u8,
+    offenders_count: u8,
 }
 
 fn spawn_traffic(
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>
+    traffic_settings: Res<TrafficSettings>,
 ) {
     let mut rng = rand::thread_rng();
-    let traffic_settings = TrafficSettings {
-        unit_speed: (0.75..1.0),
-        unit_spawn_x: (0.0..50.0),
-        unit_spawn_y: (-250.0..250.0),
-    };
 
-    for _ in 0..5 {
+    for _ in 0..traffic_settings.units_count {
         let unit_speed = rng.gen_range(traffic_settings.unit_speed.clone());
-        let position = Vec3::new(
-            rng.gen_range(traffic_settings.unit_spawn_x.clone()), 
-            rng.gen_range(traffic_settings.unit_spawn_y.clone()), 
+        let unit_color = Color::YELLOW;
+        let unit_size = Vec2::new(25f32, 25f32);
+        let unit_position = Vec3::new(
+            rng.gen_range(traffic_settings.unit_spawn_x.clone()),
+            rng.gen_range(traffic_settings.unit_spawn_y.clone()),
             0.0);
-        let traffic_unit = (MaterialMesh2dBundle {
-            mesh: meshes.add(Mesh::from(shape::Quad::new(Vec2 {
-                x: 25.0,
-                y: 25.0
-            }))).into(),
-            material: materials.add(ColorMaterial::from(Color::ORANGE_RED)),
-            transform: Transform::from_translation(position),
-            ..default()
-        },
-        TrafficUnit,
-        MovingObject {
-            speed: unit_speed
-        },
+
+        let traffic_unit = spawn_traffic_unit(
+            unit_color,
+            unit_size,
+            unit_position,
+            unit_speed,
         );
 
         commands.spawn(traffic_unit);
+    }
+
+    for _ in 0..traffic_settings.offenders_count {
+        let unit_speed = rng.gen_range(traffic_settings.offender_speed.clone());
+        let unit_color = Color::RED;
+        let unit_size = Vec2::new(25f32, 25f32);
+        let unit_position = Vec3::new(
+            rng.gen_range(traffic_settings.unit_spawn_x.clone()),
+            rng.gen_range(traffic_settings.unit_spawn_y.clone()),
+            0.0);
+
+        let offender = spawn_offender(
+            unit_color,
+            unit_size,
+            unit_position,
+            unit_speed,
+        );
+
+        commands.spawn(offender);
     }
 }
 
@@ -76,12 +112,49 @@ fn move_traffic(
 fn try_cross_dead_zone(
     mut commands: Commands,
     mut game: ResMut<Game>,
-    mut traffic_units_q: Query<(Entity, &mut Transform), With<TrafficUnit>>,
+    mut offenders_q: Query<(Entity, &mut Transform), With<Offender>>,
 ) {
-    for traffic_unit in traffic_units_q.iter_mut() {
-        if traffic_unit.1.translation.x > 1280f32 {
-            commands.entity(traffic_unit.0).despawn_recursive();
+    for offender in offenders_q.iter_mut() {
+        if offender.1.translation.x > 1280f32 {
+            commands.entity(offender.0).despawn_recursive();
             game.on_dead_zone_crossed(); 
         }
+    }
+}
+
+fn spawn_offender(
+    color: Color,
+    size: Vec2,
+    position: Vec3,
+    unit_speed: f32,
+) -> OffenderBundle {
+    return OffenderBundle {
+        traffic_unit_bundle: spawn_traffic_unit(
+            color, 
+            size, 
+            position, 
+            unit_speed),
+        offender: Offender,
+    };
+}
+
+fn spawn_traffic_unit(
+    color: Color,
+    size: Vec2,
+    position: Vec3,
+    unit_speed: f32,
+) -> TrafficUnitBundle {
+    return TrafficUnitBundle {
+        sprite_bundle: SpriteBundle {
+            sprite: Sprite { 
+                color: color,
+                custom_size: Option::Some(size),
+                ..default()
+            },
+            transform: Transform::from_translation(position),
+            ..default()
+        },
+        traffic_unit: TrafficUnit,
+        moving_object: MovingObject { speed: unit_speed }
     }
 }
